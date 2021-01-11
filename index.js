40a0bd28692a1fb444355e61cf0a7f3ca6f5c321
neoan <stefan.roehrl@gmail.com>
2021-01-11 12:38:28 -0500

    New approach

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const sass = require('node-sass');
+
+
+function renderScss(){
+  const renderedScss = sass.renderSync({
+    file: 'scss/index.scss',
+    includePaths: [__dirname +'/scss'],
+    outFile: __dirname + '/my-css.css',
+    outputStyle: 'compressed'
+  })
+
+  fs.writeFileSync(__dirname + '/my-css.css', renderedScss.css);
+}
+
+
+
+const server = http.createServer((req, res)=>{
+  if(req.url === '/'){
+    renderScss();
+
+    const html = fs.readFileSync(__dirname + '/index.html');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(html);
+  } else {
+    if(fs.existsSync(__dirname + '/' +req.url)){
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      const r = fs.readFileSync(__dirname + '/' +req.url);
+      res.write(r)
+    } else {
+      res.writeHead(404)
+    }
+
+  }
+  res.end();
+})
+server.listen(5000);
+console.log('Open http://localhost:5000')
